107a405cdcd30cbce2bd705ce0ae37ed4dbb5c88
Michael Feinberg <mfeinberg32999@gmail.com>
2016-10-31 11:42:54 -0400

    Updated printing functions

--- stat.c
+++ stat.c
@@ -36,9 +36,84 @@ int main(){
   }
 
   //Print file information
-  printf( "Size of file: %d\n" , buff -> st_size );
-  printf( "mode(permissions): %o\n" , buff -> st_mode );
+
+  double bsize = buff -> st_size;
+  double kbsize = bsize / 1000.0;
+  double mbsize = kbsize / 1000.0;
+  double gbsize = mbsize / 1000.0;
+  if(bsize < 1000){
+    printf( "Size of file: %lf B\n" , bsize );
+  }else if(bsize < 1000000){
+    printf( "Size of file: %lf KB\n" , kbsize ); 
+  }else if(bsize < 1000000000){
+    printf( "Size of file: %lf MB\n" , mbsize );
+  }else{
+    printf( "Size of file: %lf GB\n" , gbsize );
+  }
+  
+
+
   printf( "Time of last access: %s\n" , ctime(&(*buff).st_atim) );
+
+  mode_t mode = buff -> st_mode;
+  
+  printf("File permissions: ");
+  if(mode & S_IRUSR){
+    printf("r");
+  }else{
+    printf("-");
+  }
+  
+  if(mode & S_IWUSR){
+    printf("w");
+  }else{
+    printf("-");
+  }
+
+  if(mode & S_IXUSR){
+    printf("x");
+  }else{
+    printf("-");
+  }
+
+  if(mode & S_IRGRP){
+    printf("r");
+  }else{
+    printf("-");
+  }
+
+  if(mode & S_IWGRP){
+    printf("w");
+  }else{
+    printf("-");
+  }
+
+  if(mode & S_IXGRP){
+    printf("x");
+  }else{
+    printf("-");
+  }
+
+  if(mode & S_IROTH){
+    printf("r");
+  }else{
+    printf("-");
+  }
+
+  if(mode & S_IWOTH){
+    printf("w");
+  }else{
+    printf("-");
+  }
+
+  if(mode & S_IXOTH){
+    printf("x");
+  }else{
+    printf("-");
+  }
+
+  printf("\n");
+  
 
   //Free memory and close files
   free( buff );
